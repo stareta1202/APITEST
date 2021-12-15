be7fba106f1ba358db82f0d9a8d571bf9673ef70
yongjun <stareta1202@gmail.com>
2021-12-15 21:02:46 +0900

    Initial Commit

---
+++ Karrot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Karrot
+//
+//  Created by yjlee12 on 2021/12/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
